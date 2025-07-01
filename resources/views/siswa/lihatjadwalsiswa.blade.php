27b9eab950cc92575f091f1c94160feadb2e4bf1
goodman97 <151006064+goodman97@users.noreply.github.com>
2025-07-01 11:46:32 +0700

    penambahan font poopins

--- resources/views/siswa/lihatjadwalsiswa.blade.php
+++ resources/views/siswa/lihatjadwalsiswa.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Lihat Jadwal Siswa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link rel="stylesheet" href="{{ asset('css/siswa/lihatjadwalsiswa.css') }}">
 </head>
 <body>
